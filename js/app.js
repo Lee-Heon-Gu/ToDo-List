8ad8ca154ff4b7a5f7830565603922f40b615e2f
Lee-Heon-Gu <gjsrn2006@naver.com>
2023-01-15 16:32:05 +0900

    second commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 let addBtn = document.getElementById('add-btn');
 let removeAll = document.getElementById('remove-all');
 let removeBtn = document.querySelector('.remove');
@@ -11,18 +12,28 @@ inputBox.focus();
 addBtn.addEventListener('click', addTodo);
 
 function addTodo() {
-    alert('추가!');
+    // alert('추가!');
     let li = document.createElement('li');
+    li.setAttribute('data-text', inputBox.value);
+
+    let label = document.createElement('label');
+    label.classList.add('checkbox');
+
     let input = document.createElement('input');
     input.setAttribute('type', 'checkbox');
     input.classList.add('chk');
     input.addEventListener('click', completeTodo);
-    li.appendChild(input);
+    label.appendChild(input);
 
-    let span = document.createElement('span');
-    span.textContent = inputBox.value;
-    span.style.marginLeft = '5px';
-    li.appendChild(span);
+    let span1 = document.createElement('span');
+    span1.classList.add('checkbox_icon');
+    label.appendChild(span1);
+    li.appendChild(label);
+
+    let span2 = document.createElement('span');
+    span2.textContent = inputBox.value;
+    span2.style.marginLeft = '5px';
+    li.appendChild(span2);
 
     let i1 = document.createElement('i');
     i1.classList.add('fa-solid', 'fa-pencil', 'fa-2x');
@@ -75,7 +86,7 @@ removeBtn.addEventListener('click', removeParentNode);
 modifyBtn.addEventListener('click', modifyText);
 
 function modifyText(event) {
-    alert('수정!');
+    // alert('수정!');
     let textInput = prompt('수정할 텍스트 입력');
 
     if (textInput !== null)
@@ -85,7 +96,7 @@ function modifyText(event) {
 };
 
 function removeParentNode(event) {
-    alert('삭제!');
+    // alert('삭제!');
     event.currentTarget.parentNode.remove();
     inputBox.focus();
 };
@@ -106,10 +117,38 @@ function completeTodo(event) {
     if (event.target.checked === true)
     {
         alert('complete!');
-        event.target.nextElementSibling.classList.add('complete');
+        event.target.parentNode.nextElementSibling.classList.add('complete');
     }
     else
     {
-        event.target.nextElementSibling.classList.remove('complete');
+        event.target.parentNode.nextElementSibling.classList.remove('complete');
     }
 };
+
+// 브라우저 종료 전에 저장
+window.addEventListener('beforeunload', function () {
+    let dataSave = new Array(0);
+    let liList = todoList.children;
+    for (let i = 0; i < liList.length; i++)
+    {
+        dataSave.push(liList[i].dataset.text);
+    }
+    let stringArr = JSON.stringify(dataSave);
+    localStorage.setItem('todo', stringArr);
+});
+
+// 브라우저 시작 시 불러오기
+function getTodo() {
+    let dataSave = JSON.parse(localStorage.getItem('todo'));
+    if (dataSave.length > 0)
+    {
+        todoList.firstElementChild.remove();
+        for (let i = 0; i < dataSave.length; i++)
+        {
+            inputBox.value = dataSave[i];
+            addTodo();
+        }
+    }
+};
+
+getTodo();
